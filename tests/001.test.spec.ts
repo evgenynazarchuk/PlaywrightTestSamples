afdfa326e0b23ba26e82d03e02c10040c2769fa7
Evgeny Nazarchuk <68129901+evgenynazarchuk@users.noreply.github.com>
2022-01-06 17:59:26 +0300

    test

--- tests/001.test.spec.ts
+++ tests/001.test.spec.ts
@@ -7,5 +7,5 @@ test('001_1.test.spec.ts', async ({ page }) => {
 
 test('001_2.test.spec.ts', async ({ page }) => {
     await page.goto('https://playwright.dev/');
-    expect(await page.screenshot()).toMatchSnapshot('001_2.test.page.png');
+    expect(await page.screenshot()).toMatchSnapshot('001_2.test.page.png', { threshold: 0.2 });
 })
