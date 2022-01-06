bf621a916899b6856cd8e8b1c6bea3c1431db7b4
Evgeny Nazarchuk <68129901+evgenynazarchuk@users.noreply.github.com>
2022-01-06 16:49:39 +0300

    .

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ const config: PlaywrightTestConfig = {
   use: {
     trace: 'on-first-retry',
   },
+  reporter: [ ['html', { outputFolder: 'html-report' }] ],
   projects: [
     {
       name: 'chromium',
