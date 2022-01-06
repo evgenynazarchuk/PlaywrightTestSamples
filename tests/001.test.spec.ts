58b73068a9515f871f8e86d08d00cc7f53777493
Evgeny Nazarchuk <68129901+evgenynazarchuk@users.noreply.github.com>
2022-01-06 17:19:25 +0300

    test

--- tests/001.test.spec.ts
+++ tests/001.test.spec.ts
@@ -5,7 +5,7 @@ test('001_1.test.spec.ts', async ({ page }) => {
     await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright');
 })
 
-// test('001_2.test.spec.ts', async ({ page }) => {
-//     await page.goto('https://playwright.dev/');
-//     expect(await page.screenshot()).toMatchSnapshot('001_2.test.page.png');
-// })
+test('001_2.test.spec.ts', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+    expect(await page.screenshot()).toMatchSnapshot('001_2.test.page.png');
+})
