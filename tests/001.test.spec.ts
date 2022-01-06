26a4b022ae198341ce7575268423fbe892ac75a6
Evgeny Nazarchuk <68129901+evgenynazarchuk@users.noreply.github.com>
2022-01-06 18:07:55 +0300

    test

--- tests/001.test.spec.ts
+++ tests/001.test.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test'
 
-test('001_1.test.spec.ts', async ({ page }) => {
+test('test main page title', async ({ page }) => {
     await page.goto('https://playwright.dev/');
     await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright');
 })
 
-test('001_2.test.spec.ts', async ({ page }) => {
+test('test main page screenshot', async ({ page }) => {
     await page.goto('https://playwright.dev/');
-    expect(await page.screenshot()).toMatchSnapshot('001_2.test.page.png', { threshold: 0.2 });
+    expect(await page.screenshot()).toMatchSnapshot('main page.png', { threshold: 0.2 });
 })
