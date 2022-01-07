2f7b370d9080a7837e73e21629a4258aa587bf9b
Evgeny Nazarchuk <68129901+evgenynazarchuk@users.noreply.github.com>
2022-01-08 01:31:48 +0300

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,17 @@ import { PlaywrightTestConfig, devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 3 : 0,
-    workers: process.env.CI ? 6 : 6,
+    workers: process.env.CI ? 6 : undefined,
+    timeout: 30 * 1000,
+    globalTimeout: 60 * 60 * 1000,
+    expect: {
+      timeout: 10 * 1000,
+      toMatchSnapshot: { threshold: 0.1 }
+    },
     use: {
       trace: 'on-first-retry',
+      actionTimeout: 10 * 1000,
+      navigationTimeout: 20 * 1000
     },
     reporter: [ ['html', { outputFolder: 'playwright-report' }] ],
     projects: [
